03343a8615daa1dbda4dcde603843826f0048467
akihiko47 <akihiko4701a@gmail.com>
2023-11-10 22:03:17 +0300

    Some modifications

--- src/components/TiltElement.jsx
+++ src/components/TiltElement.jsx
@@ -5,15 +5,15 @@ function TiltElement( { glareEnable } ) {
   return (
     <Tilt
     className="h-full rounded-lg w-full aspect-square overflow-hidden group"
-    perspective={500}
+    perspective={800}
     glareEnable={true}
     glareMaxOpacity={0.45}
     scale={1.02}
   >
     <img src='./assets/banger.jpg' className='group-hover:scale-125 transition ease-in-out duration-1000'></img>
-    <div className='absolute left-0 top-0 h-full w-full bg-background-900 bg-opacity-80'>
+    <div className='absolute left-0 top-0 h-full w-full bg-background-900 bg-opacity-90 group-hover:bg-opacity-60 transition-all ease-in-out duration-500'>
       <div className='flex h-full items-center justify-center inner-element'>
-        <div className='font-bold text-xl text-center text-primary-300 md:text-2xl transition-all ease-in-out duration-300 group-hover:text-2xl'>Banger</div>
+        <div className='font-bold text-xl text-center text-primary-300 md:text-2xl transition-all ease-in-out duration-300 group-hover:text-2xl uppercase'>Banger</div>
       </div>
     </div>
 
