034e24219cf08a6e8ff90f3aaa602e3eb1590c1f
akihiko47 <akihiko4701a@gmail.com>
2023-11-11 20:25:21 +0300

    Fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,10 +24,10 @@ function Hero() {
     <div className='flex p-2 flex-col text-center w-full h-[80vh] min-h-[700px] max-h-[1000px] max-w-[400px] mx-auto justify-center md:max-w-[1200px] md:p-10'>
         <h1 className='text-3xl text-transparent bg-clip-text bg-gradient-to-br from-text-50 to-primary-200 md:text-4xl md:text-left mt-28 leading-none md:w-[55%] uppercase font-bold'>this is a page with my projects</h1>
         <p className='text-text-50 text-md mt-5 md:text-lg md:w-[55%] md:text-left'>This is my first tailwind page. This page contains my projects with links to github. Hope you like it!</p>
-        <div className='flex justify-center mt-20 md:absolute md:left-[65%] md:top-[25%]'>
+        <div className='flex justify-center mt-20 md:absolute md:left-[65%] md:top-[30%]'>
             <GiAbstract050 size="200px" className='text-background-900' style = {{transform: `rotate(${angle}deg)`}}/>
         </div>
-        <button type='button' onClick={scrollToProjects} className='transition-all ease-in-out duration-200 uppercase bg-primary-300 px-6 py-3 rounded-md mx-auto font-bold mt-14 hover:rounded-3xl hover:bg-primary-200 hover:shadow-md hover:shadow-primary-300 hover:-translate-y-2 md:ml-0'>view projects</button>
+        <button type='button' onClick={scrollToProjects} className='transition-all ease-in-out duration-200 uppercase bg-primary-300 px-6 py-3 rounded-md mx-auto font-bold mt-14 hover:rounded-3xl hover:bg-primary-200 hover:shadow-md hover:shadow-primary-300 hover:-translate-y-2 md:ml-0 md:mt-8'>view projects</button>
     </div>
     
   )
