9251aa428b07f0a3b95702e3282ab096ccb0aa86
akihiko47 <akihiko4701a@gmail.com>
2023-11-11 12:42:13 +0300

    new background pattern

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import "./index.css"
 function App() {
   return (
     <div className="h-full overflow-hidden w-full bg-background-950">
+      <div className="absolute h-full w-full"><div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:18px_28px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div></div>
       <NavBar/>
       <Hero/>
       <Main/>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,13 +13,12 @@ function Hero() {
     }, [angle]);
 
   return (
-    <div className='flex p-2 flex-col text-center w-full h-screen pattern-cross pattern-background-940 pattern-bg-background-950 
-    pattern-size-24 pattern-opacity-100'>
-        <p className='text-primary-200 text-xl mt-20'>Welocome to</p>
+    <div className='flex p-2 flex-col text-center w-full h-screen min-h-[800px]'>
+        <p className='text-primary-200 text-xl mt-32'>Welocome to</p>
         <h1 className='text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-primary-100 to-primary-800 md:text-5xl'>my projects</h1>
         <p className='text-primary-200 text-xl mt-5'>I hope you like them!</p>
-        <div className='flex justify-center mt-20 blur-md opacity-25'>
-            <TbHexagonLetterA size="300px" className='text-primary-300' style = {{transform: `rotate(${angle}deg)`}}/>
+        <div className='flex justify-center mt-20'>
+            <TbHexagonLetterA size="300px" className='text-secondary-300' style = {{transform: `rotate(${angle}deg)`}}/>
         </div>
         <button type='button' className='transition-all ease-in-out duration-200 uppercase bg-primary-300 px-6 py-3 rounded-md mx-auto font-extrabold mt-14 hover:rounded-3xl hover:bg-primary-200 hover:shadow-md hover:shadow-primary-300 hover:-translate-y-2'>view projects</button>
     </div>
